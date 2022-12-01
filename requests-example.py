4d7000702c65d06a4e19960cb3dec249d3f0f37b
Jon Craton <jncraton@gmail.com>
2022-12-01 12:15:55 -0500

    Update requests example

--- requests-example.py
+++ requests-example.py
@@ -1,13 +1,8 @@
 import requests
-import json
 import sys
 
-text = ' '.join(sys.argv[1:])
+sentence = " ".join(sys.argv[1:])
 
-response = requests.post(
-    "https://sentim-api.herokuapp.com/api/v1/",
-    headers={"accept": "application/json", "content-type": "application/json"},
-    data=json.dumps({"text": text})
-)
+response = requests.post("https://joncraton.com/sentiment", data=sentence)
 
-print(response.json()["result"]["polarity"])
+print(response.text)
